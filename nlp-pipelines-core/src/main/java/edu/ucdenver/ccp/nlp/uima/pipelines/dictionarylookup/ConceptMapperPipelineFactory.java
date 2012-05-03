8550105d9b3d56ad97968173d690dab8786f5c76
Bill Baumgartner <bill.baumgartner@gmail.com>
2012-05-03 21:25:17 +0000

    Now makes use of the ConceptMapperPermutationFactory in getPipelineAeDescriptions()

--- nlp-pipelines-core/src/main/java/edu/ucdenver/ccp/nlp/uima/pipelines/dictionarylookup/ConceptMapperPipelineFactory.java
+++ nlp-pipelines-core/src/main/java/edu/ucdenver/ccp/nlp/uima/pipelines/dictionarylookup/ConceptMapperPipelineFactory.java
@@ -11,7 +11,6 @@ import java.util.List;
 import org.apache.log4j.Logger;
 import org.apache.uima.UIMAException;
 import org.apache.uima.analysis_engine.AnalysisEngineDescription;
-import org.apache.uima.jcas.tcas.Annotation;
 import org.apache.uima.resource.metadata.TypeSystemDescription;
 
 import edu.ucdenver.ccp.common.collections.CollectionsUtil;
@@ -21,9 +20,7 @@ import edu.ucdenver.ccp.nlp.core.mention.ClassMentionTypes;
 import edu.ucdenver.ccp.nlp.ext.uima.annotators.filter.ClassMentionRemovalFilter_AE;
 import edu.ucdenver.ccp.nlp.uima.pipelines.dictionarylookup.ConceptMapperDictionaryFileFactory.DictionaryNamespace;
 import edu.ucdenver.ccp.nlp.uima.pipelines.dictionarylookup.ConceptMapperPipelineCmdOpts.DictionaryParameterOperation;
-import edu.ucdenver.ccp.nlp.wrapper.conceptmapper.ConceptMapperAggregateFactory;
-import edu.ucdenver.ccp.nlp.wrapper.conceptmapper.ConceptMapperFactory.SearchStrategyParamValue;
-import edu.ucdenver.ccp.nlp.wrapper.conceptmapper.ConceptMapperFactory.TokenNormalizerConfigParam.CaseMatchParamValue;
+import edu.ucdenver.ccp.nlp.wrapper.conceptmapper.ConceptMapperPermutationFactory;
 import edu.ucdenver.ccp.nlp.wrapper.conceptmapper.typesystem.ConceptMapper2CCPTypeSystemConverter_AE;
 
 /**
@@ -42,6 +39,49 @@ public class ConceptMapperPipelineFactory {
 			"edu.ucdenver.ccp.nlp.wrapper.conceptmapper.TypeSystem", "analysis_engine.primitive.DictTerm",
 			"org.apache.uima.conceptMapper.support.tokenizer.TokenAnnotation");
 
+	// /**
+	// * Returns an aggregate: sentence detector, offset tokenizer, conceptmapper
+	// *
+	// * @param tsd
+	// * @param cmdOptions
+	// * @return
+	// * @throws IOException
+	// * @throws UIMAException
+	// */
+	// public static List<AnalysisEngineDescription> getPipelineAeDescriptions(TypeSystemDescription
+	// tsd,
+	// ConceptMapperPipelineCmdOpts cmdOptions) throws UIMAException, IOException {
+	//
+	// File cmDictionaryFile = cmdOptions.getDictionaryFile();
+	// FileUtil.validateFile(cmDictionaryFile);
+	//
+	// CaseMatchParamValue caseMatchParamValue = CaseMatchParamValue.CASE_FOLD_DIGITS;
+	// SearchStrategyParamValue searchStrategyParamValue =
+	// SearchStrategyParamValue.CONTIGUOUS_MATCH;
+	// Class<? extends Annotation> spanFeatureStructureClass = cmdOptions.getSpanClass();
+	// AnalysisEngineDescription conceptMapperAggregateDesc = ConceptMapperAggregateFactory
+	// .getOffsetTokenizerConceptMapperPipelineDescription(tsd, cmDictionaryFile,
+	// caseMatchParamValue,
+	// searchStrategyParamValue, spanFeatureStructureClass);
+	//
+	// /* Converts from the CM OntologyTerm annotation class to CCPTextAnnotation classes */
+	// AnalysisEngineDescription cmToCcpTypeSystemConverterDesc =
+	// ConceptMapper2CCPTypeSystemConverter_AE
+	// .createAnalysisEngineDescription(tsd);
+	//
+	// /* Removes all token annotations as we don't want them to be output as RDF */
+	// AnalysisEngineDescription tokenRemovalDesc =
+	// ClassMentionRemovalFilter_AE.createAnalysisEngineDescription(tsd,
+	// new String[] { ClassMentionTypes.TOKEN });
+	//
+//		/* @formatter:off */
+//		return CollectionsUtil.createList(
+//				conceptMapperAggregateDesc,
+//				cmToCcpTypeSystemConverterDesc,
+//				tokenRemovalDesc); 
+//		/* @formatter:on */
+	// }
+
 	/**
 	 * Returns an aggregate: sentence detector, offset tokenizer, conceptmapper
 	 * 
@@ -52,17 +92,14 @@ public class ConceptMapperPipelineFactory {
 	 * @throws UIMAException
 	 */
 	public static List<AnalysisEngineDescription> getPipelineAeDescriptions(TypeSystemDescription tsd,
-			ConceptMapperPipelineCmdOpts cmdOptions) throws UIMAException, IOException {
+			ConceptMapperPipelineCmdOpts cmdOptions, int parameterValuesIndex) throws UIMAException, IOException {
 
 		File cmDictionaryFile = cmdOptions.getDictionaryFile();
 		FileUtil.validateFile(cmDictionaryFile);
 
-		CaseMatchParamValue caseMatchParamValue = CaseMatchParamValue.CASE_FOLD_DIGITS;
-		SearchStrategyParamValue searchStrategyParamValue = SearchStrategyParamValue.CONTIGUOUS_MATCH;
-		Class<? extends Annotation> spanFeatureStructureClass = cmdOptions.getSpanClass();
-		AnalysisEngineDescription conceptMapperAggregateDesc = ConceptMapperAggregateFactory
-				.getOffsetTokenizerConceptMapperPipelineDescription(tsd, cmDictionaryFile, caseMatchParamValue,
-						searchStrategyParamValue, spanFeatureStructureClass);
+		AnalysisEngineDescription conceptMapperAggregateDesc = ConceptMapperPermutationFactory
+				.buildConceptMapperAggregatePermutation(parameterValuesIndex, tsd, cmDictionaryFile,
+						cmdOptions.getSpanClass());
 
 		/* Converts from the CM OntologyTerm annotation class to CCPTextAnnotation classes */
 		AnalysisEngineDescription cmToCcpTypeSystemConverterDesc = ConceptMapper2CCPTypeSystemConverter_AE
@@ -82,27 +119,36 @@ public class ConceptMapperPipelineFactory {
 
 	public static List<AnalysisEngineDescription> getPipelineAeDescriptions(DictionaryNamespace dictNamespace,
 			TypeSystemDescription tsd, ConceptMapperPipelineCmdOpts cmdOptions,
-			DictionaryParameterOperation dictParamOp, CleanDirectory workDirectoryOp) {
+			DictionaryParameterOperation dictParamOp, CleanDirectory workDirectoryOp, int parameterCombinationIndex) {
 		try {
 			switch (dictNamespace) {
 			case CHEBI:
-				return getChebiPipelineAeDescriptions(tsd, cmdOptions, dictParamOp, workDirectoryOp);
+				return getChebiPipelineAeDescriptions(tsd, cmdOptions, dictParamOp, workDirectoryOp,
+						parameterCombinationIndex);
 			case CL:
-				return getCellTypePipelineAeDescriptions(tsd, cmdOptions, dictParamOp, workDirectoryOp);
+				return getCellTypePipelineAeDescriptions(tsd, cmdOptions, dictParamOp, workDirectoryOp,
+						parameterCombinationIndex);
 			case SO:
-				return getSequenceOntologyPipelineAeDescriptions(tsd, cmdOptions, dictParamOp, workDirectoryOp);
+				return getSequenceOntologyPipelineAeDescriptions(tsd, cmdOptions, dictParamOp, workDirectoryOp,
+						parameterCombinationIndex);
 			case PR:
-				return getProteinOntologyPipelineAeDescriptions(tsd, cmdOptions, dictParamOp, workDirectoryOp);
+				return getProteinOntologyPipelineAeDescriptions(tsd, cmdOptions, dictParamOp, workDirectoryOp,
+						parameterCombinationIndex);
 			case GO_CC:
-				return getGoCcPipelineAeDescriptions(tsd, cmdOptions, dictParamOp, workDirectoryOp);
+				return getGoCcPipelineAeDescriptions(tsd, cmdOptions, dictParamOp, workDirectoryOp,
+						parameterCombinationIndex);
 			case GO_BP:
-				return getGoBpPipelineAeDescriptions(tsd, cmdOptions, dictParamOp, workDirectoryOp);
+				return getGoBpPipelineAeDescriptions(tsd, cmdOptions, dictParamOp, workDirectoryOp,
+						parameterCombinationIndex);
 			case GO_MF:
-				return getGoMfPipelineAeDescriptions(tsd, cmdOptions, dictParamOp, workDirectoryOp);
+				return getGoMfPipelineAeDescriptions(tsd, cmdOptions, dictParamOp, workDirectoryOp,
+						parameterCombinationIndex);
 			case GO_BPMF:
-				return getGoBpMfPipelineAeDescriptions(tsd, cmdOptions, dictParamOp, workDirectoryOp);
+				return getGoBpMfPipelineAeDescriptions(tsd, cmdOptions, dictParamOp, workDirectoryOp,
+						parameterCombinationIndex);
 			case NCBI_TAXON:
-				return getNcbiTaxonPipelineAeDescriptions(tsd, cmdOptions, dictParamOp, workDirectoryOp);
+				return getNcbiTaxonPipelineAeDescriptions(tsd, cmdOptions, dictParamOp, workDirectoryOp,
+						parameterCombinationIndex);
 			default:
 				throw new IllegalArgumentException("DictionaryNamespace." + dictNamespace.name()
 						+ " is not currently handled by the getPipelineAeDescriptions() method.");
@@ -116,67 +162,78 @@ public class ConceptMapperPipelineFactory {
 
 	public static List<AnalysisEngineDescription> getEntrezGenePipelineAeDescriptions(TypeSystemDescription tsd,
 			ConceptMapperPipelineCmdOpts cmdOptions, DictionaryParameterOperation dictParamOp,
-			CleanDirectory workDirectoryOp) throws UIMAException, IOException {
-		return getPipelineAeDescriptions(tsd, cmdOptions, dictParamOp, DictionaryNamespace.EG, workDirectoryOp);
+			CleanDirectory workDirectoryOp, int parameterCombinationIndex) throws UIMAException, IOException {
+		return getPipelineAeDescriptions(tsd, cmdOptions, dictParamOp, DictionaryNamespace.EG, workDirectoryOp,
+				parameterCombinationIndex);
 	}
 
 	public static List<AnalysisEngineDescription> getCellTypePipelineAeDescriptions(TypeSystemDescription tsd,
 			ConceptMapperPipelineCmdOpts cmdOptions, DictionaryParameterOperation dictParamOp,
-			CleanDirectory workDirectoryOp) throws UIMAException, IOException {
-		return getPipelineAeDescriptions(tsd, cmdOptions, dictParamOp, DictionaryNamespace.CL, workDirectoryOp);
+			CleanDirectory workDirectoryOp, int parameterCombinationIndex) throws UIMAException, IOException {
+		return getPipelineAeDescriptions(tsd, cmdOptions, dictParamOp, DictionaryNamespace.CL, workDirectoryOp,
+				parameterCombinationIndex);
 	}
 
 	public static List<AnalysisEngineDescription> getChebiPipelineAeDescriptions(TypeSystemDescription tsd,
 			ConceptMapperPipelineCmdOpts cmdOptions, DictionaryParameterOperation dictParamOp,
-			CleanDirectory workDirectoryOp) throws UIMAException, IOException {
-		return getPipelineAeDescriptions(tsd, cmdOptions, dictParamOp, DictionaryNamespace.CHEBI, workDirectoryOp);
+			CleanDirectory workDirectoryOp, int parameterCombinationIndex) throws UIMAException, IOException {
+		return getPipelineAeDescriptions(tsd, cmdOptions, dictParamOp, DictionaryNamespace.CHEBI, workDirectoryOp,
+				parameterCombinationIndex);
 	}
 
 	public static List<AnalysisEngineDescription> getSequenceOntologyPipelineAeDescriptions(TypeSystemDescription tsd,
 			ConceptMapperPipelineCmdOpts cmdOptions, DictionaryParameterOperation dictParamOp,
-			CleanDirectory workDirectoryOp) throws UIMAException, IOException {
-		return getPipelineAeDescriptions(tsd, cmdOptions, dictParamOp, DictionaryNamespace.SO, workDirectoryOp);
+			CleanDirectory workDirectoryOp, int parameterCombinationIndex) throws UIMAException, IOException {
+		return getPipelineAeDescriptions(tsd, cmdOptions, dictParamOp, DictionaryNamespace.SO, workDirectoryOp,
+				parameterCombinationIndex);
 	}
 
 	public static List<AnalysisEngineDescription> getProteinOntologyPipelineAeDescriptions(TypeSystemDescription tsd,
 			ConceptMapperPipelineCmdOpts cmdOptions, DictionaryParameterOperation dictParamOp,
-			CleanDirectory workDirectoryOp) throws UIMAException, IOException {
-		return getPipelineAeDescriptions(tsd, cmdOptions, dictParamOp, DictionaryNamespace.PR, workDirectoryOp);
+			CleanDirectory workDirectoryOp, int parameterCombinationIndex) throws UIMAException, IOException {
+		return getPipelineAeDescriptions(tsd, cmdOptions, dictParamOp, DictionaryNamespace.PR, workDirectoryOp,
+				parameterCombinationIndex);
 	}
 
 	public static List<AnalysisEngineDescription> getGoCcPipelineAeDescriptions(TypeSystemDescription tsd,
 			ConceptMapperPipelineCmdOpts cmdOptions, DictionaryParameterOperation dictParamOp,
-			CleanDirectory workDirectoryOp) throws UIMAException, IOException {
-		return getPipelineAeDescriptions(tsd, cmdOptions, dictParamOp, DictionaryNamespace.GO_CC, workDirectoryOp);
+			CleanDirectory workDirectoryOp, int parameterCombinationIndex) throws UIMAException, IOException {
+		return getPipelineAeDescriptions(tsd, cmdOptions, dictParamOp, DictionaryNamespace.GO_CC, workDirectoryOp,
+				parameterCombinationIndex);
 	}
 
 	public static List<AnalysisEngineDescription> getGoBpPipelineAeDescriptions(TypeSystemDescription tsd,
 			ConceptMapperPipelineCmdOpts cmdOptions, DictionaryParameterOperation dictParamOp,
-			CleanDirectory workDirectoryOp) throws UIMAException, IOException {
-		return getPipelineAeDescriptions(tsd, cmdOptions, dictParamOp, DictionaryNamespace.GO_BP, workDirectoryOp);
+			CleanDirectory workDirectoryOp, int parameterCombinationIndex) throws UIMAException, IOException {
+		return getPipelineAeDescriptions(tsd, cmdOptions, dictParamOp, DictionaryNamespace.GO_BP, workDirectoryOp,
+				parameterCombinationIndex);
 	}
 
 	public static List<AnalysisEngineDescription> getGoMfPipelineAeDescriptions(TypeSystemDescription tsd,
 			ConceptMapperPipelineCmdOpts cmdOptions, DictionaryParameterOperation dictParamOp,
-			CleanDirectory workDirectoryOp) throws UIMAException, IOException {
-		return getPipelineAeDescriptions(tsd, cmdOptions, dictParamOp, DictionaryNamespace.GO_MF, workDirectoryOp);
+			CleanDirectory workDirectoryOp, int parameterCombinationIndex) throws UIMAException, IOException {
+		return getPipelineAeDescriptions(tsd, cmdOptions, dictParamOp, DictionaryNamespace.GO_MF, workDirectoryOp,
+				parameterCombinationIndex);
 	}
 
 	public static List<AnalysisEngineDescription> getGoBpMfPipelineAeDescriptions(TypeSystemDescription tsd,
 			ConceptMapperPipelineCmdOpts cmdOptions, DictionaryParameterOperation dictParamOp,
-			CleanDirectory workDirectoryOp) throws UIMAException, IOException {
-		return getPipelineAeDescriptions(tsd, cmdOptions, dictParamOp, DictionaryNamespace.GO_BPMF, workDirectoryOp);
+			CleanDirectory workDirectoryOp, int parameterCombinationIndex) throws UIMAException, IOException {
+		return getPipelineAeDescriptions(tsd, cmdOptions, dictParamOp, DictionaryNamespace.GO_BPMF, workDirectoryOp,
+				parameterCombinationIndex);
 	}
 
 	public static List<AnalysisEngineDescription> getNcbiTaxonPipelineAeDescriptions(TypeSystemDescription tsd,
 			ConceptMapperPipelineCmdOpts cmdOptions, DictionaryParameterOperation dictParamOp,
-			CleanDirectory workDirectoryOp) throws UIMAException, IOException {
-		return getPipelineAeDescriptions(tsd, cmdOptions, dictParamOp, DictionaryNamespace.NCBI_TAXON, workDirectoryOp);
+			CleanDirectory workDirectoryOp, int parameterCombinationIndex) throws UIMAException, IOException {
+		return getPipelineAeDescriptions(tsd, cmdOptions, dictParamOp, DictionaryNamespace.NCBI_TAXON, workDirectoryOp,
+				parameterCombinationIndex);
 	}
 
 	private static List<AnalysisEngineDescription> getPipelineAeDescriptions(TypeSystemDescription tsd,
 			ConceptMapperPipelineCmdOpts cmdOptions, DictionaryParameterOperation dictParamOp,
-			DictionaryNamespace oboNamespace, CleanDirectory workDirectoryOp) throws UIMAException, IOException {
+			DictionaryNamespace oboNamespace, CleanDirectory workDirectoryOp, int parameterCombinationIndex)
+			throws UIMAException, IOException {
 
 		File workDirectory = null;
 		if (dictParamOp.equals(DictionaryParameterOperation.IGNORE)) {
@@ -194,7 +251,7 @@ public class ConceptMapperPipelineFactory {
 			cmdOptions.setDictionaryFile(cmDictFile);
 		}
 
-		return getPipelineAeDescriptions(tsd, cmdOptions);
+		return getPipelineAeDescriptions(tsd, cmdOptions, parameterCombinationIndex);
 	}
 
 }
