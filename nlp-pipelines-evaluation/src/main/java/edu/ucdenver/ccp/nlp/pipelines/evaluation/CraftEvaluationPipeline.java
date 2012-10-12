189fc1e67df0a70325e8227b95db4610af88238b
Bill Baumgartner <bill.baumgartner@gmail.com>
2012-10-12 23:07:48 +0000

    updated to use the craft-code classes

--- nlp-pipelines-evaluation/src/main/java/edu/ucdenver/ccp/nlp/pipelines/evaluation/CraftEvaluationPipeline.java
+++ nlp-pipelines-evaluation/src/main/java/edu/ucdenver/ccp/nlp/pipelines/evaluation/CraftEvaluationPipeline.java
@@ -6,15 +6,17 @@ package edu.ucdenver.ccp.nlp.pipelines.evaluation;
 import java.util.Collection;
 import java.util.Set;
 
+import org.apache.uima.collection.CollectionReader;
+import org.apache.uima.collection.CollectionReaderDescription;
 import org.apache.uima.resource.ResourceInitializationException;
 import org.apache.uima.resource.metadata.TypeSystemDescription;
+import org.uimafit.factory.CollectionReaderFactory;
 
+import edu.ucdenver.ccp.craft.CraftConceptType;
+import edu.ucdenver.ccp.craft.CraftRelease;
+import edu.ucdenver.ccp.craft.uima.cr.CcpCraftCollectionReader;
 import edu.ucdenver.ccp.nlp.ext.uima.annotators.comparison.AnnotationComparator_AE.MentionComparatorType;
 import edu.ucdenver.ccp.nlp.ext.uima.annotators.comparison.AnnotationComparator_AE.SpanComparatorType;
-import edu.ucdenver.ccp.nlp.pipelines.evaluation.CraftPipelineUtil.CraftAnnotationFilterOp;
-import edu.ucdenver.ccp.nlp.pipelines.evaluation.CraftPipelineUtil.CraftConceptType;
-import edu.ucdenver.ccp.nlp.pipelines.evaluation.CraftPipelineUtil.CraftVersion;
-import edu.ucdenver.ccp.uima.shims.document.DocumentMetadataHandler;
 
 /**
  * @author Colorado Computational Pharmacology, UC Denver; ccpsupport@ucdenver.edu
@@ -23,24 +25,33 @@ import edu.ucdenver.ccp.uima.shims.document.DocumentMetadataHandler;
 public class CraftEvaluationPipeline extends EvaluationPipeline {
 
 	/**
-	 * @param craftVersion
+	 * @param craftRelease
 	 * @param conceptTypesToLoad
 	 * @param spanComparatorType
 	 * @param mentionComparatorType
 	 * @param tsd
-	 * @param documentMetadataExtractorClass
+	 * @param annotationTypeRegexes
 	 * @throws ResourceInitializationException
 	 */
-	public CraftEvaluationPipeline(CraftVersion craftVersion, Set<CraftConceptType> conceptTypesToLoad,
+	public CraftEvaluationPipeline(CraftRelease craftRelease, Set<CraftConceptType> conceptTypesToLoad,
 			TypeSystemDescription tsd, SpanComparatorType spanComparatorType,
-			MentionComparatorType mentionComparatorType,
-			Class<? extends DocumentMetadataHandler> documentMetadataExtractorClass,
-			Collection<String> annotationTypeRegexes, CraftAnnotationFilterOp annotFilterOp)
+			MentionComparatorType mentionComparatorType, Collection<String> annotationTypeRegexes)
 			throws ResourceInitializationException {
-		super(tsd, CraftPipelineUtil.getCraftCollectionReader(craftVersion, tsd, documentMetadataExtractorClass),
-				CraftPipelineUtil.getCraftAnnotationLoaderDescriptions(craftVersion, conceptTypesToLoad, tsd,
-						documentMetadataExtractorClass, annotFilterOp), spanComparatorType, mentionComparatorType,
-				annotationTypeRegexes);
+		super(tsd, getCraftCollectionReader(craftRelease, conceptTypesToLoad), spanComparatorType,
+				mentionComparatorType, annotationTypeRegexes);
+	}
+
+	/**
+	 * @param craftRelease
+	 * @param conceptTypesToLoad
+	 * @return a {@link CollectionReader} component for the specified {@link CraftRelease}
+	 *         initialized to include the {@link CraftConceptType}s noted in the input
+	 * @throws ResourceInitializationException
+	 */
+	private static CollectionReader getCraftCollectionReader(CraftRelease craftRelease,
+			Set<CraftConceptType> conceptTypesToLoad) throws ResourceInitializationException {
+		CollectionReaderDescription crDesc = CcpCraftCollectionReader.getDescription(craftRelease, conceptTypesToLoad);
+		return CollectionReaderFactory.createCollectionReader(crDesc);
 	}
 
 }
