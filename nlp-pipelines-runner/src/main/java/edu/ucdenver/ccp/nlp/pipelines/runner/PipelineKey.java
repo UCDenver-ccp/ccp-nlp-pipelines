945633b0170922aa23a15435cc0e6d3575275380
bill-baumgartner <bill.baumgartner@gmail.com>
2017-02-20 22:29:20 -0700

    Added conceptmapper key to the PipelineKey enum

--- nlp-pipelines-runner/src/main/java/edu/ucdenver/ccp/nlp/pipelines/runner/PipelineKey.java
+++ nlp-pipelines-runner/src/main/java/edu/ucdenver/ccp/nlp/pipelines/runner/PipelineKey.java
@@ -1,7 +1,8 @@
 package edu.ucdenver.ccp.nlp.pipelines.runner;
 
 public enum PipelineKey {
-	XML2TXT(0);
+	XML2TXT(0),
+	CONCEPTMAPPER(1);
 
 	/*
 	 * Priority provides a simple way to order pipeline runs. Integer values
